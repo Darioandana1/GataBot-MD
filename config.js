4ac9bdb87c1ae8c46d44ca97a75b46e7aeb40ce4
Darioandana1 <vergararuben30@gmail.com>
2024-09-28 03:44:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ global.owner = [
 ["595971490733"],
 ["5215539356057"]]
 
-global.mods = []
+global.mods = [5492645880516]
 global.prems = []
 
 // Cambiar a false para usar el Bot desde el mismo numero del Bot.
